7cbe8c9f84509e3439317fbe6eecff50cac383a3
shaykatWorks <shaykatworks@gmail.com>
2022-02-08 02:19:38 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.text_slick').slick({
+        infinite: true,
+        cssEase: 'linear',
+        autoplay: 500,
+        dots:true,
+        arrows:true,
+        // fade: true,
+        cssEase: 'linear',
+    
+      })
+    });
+
+
+
+var typed3 = new Typed('.typing', {
+  strings: ['Graphic  Design ','Developer'],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
+
+
+new WOW().init();
+    
